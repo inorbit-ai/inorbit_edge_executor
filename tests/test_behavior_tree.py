447b158979dc6907ee45f781bffea47d44307c50
Miguel Garcia <miguel.garcia@gmail.com>
2025-08-18 13:30:02 -0300

    fix tests

--- tests/test_behavior_tree.py
+++ tests/test_behavior_tree.py
@@ -1,8 +1,7 @@
 from logging import getLogger
 
 import pytest
-from inorbit_edge_executor.behavior_tree import BehaviorTree
-from inorbit_edge_executor.behavior_tree import BehaviorTreeBuilderContext
+from inorbit_edge_executor.behavior_tree import BehaviorTree, DefaultTreeBuilder, BehaviorTreeBuilderContext
 from inorbit_edge_executor.datatypes import MissionDefinition
 from inorbit_edge_executor.datatypes import MissionRuntimeOptions
 from inorbit_edge_executor.datatypes import MissionRuntimeSharedMemory
@@ -10,7 +9,6 @@ from inorbit_edge_executor.dummy_backend import DummyDB
 from inorbit_edge_executor.inorbit import InOrbitAPI
 from inorbit_edge_executor.inorbit import RobotApiFactory
 from inorbit_edge_executor.mission import Mission
-from inorbit_edge_executor.worker_pool import WorkerPool
 
 logger = getLogger("test-bt")
 
@@ -25,7 +23,6 @@ def test_bt_build_simple(inorbit_api):
     This test only builds a basic mission tree with no steps, to validate its 'skeleton': the
     error handlers, mission start/completion nodes, etc. No actual steps are validated.
     """
-    worker_pool = WorkerPool(db=DummyDB(), api=inorbit_api)
     mission: Mission = Mission(
         id="mission123",
         robot_id="robot123",
@@ -35,77 +32,85 @@ def test_bt_build_simple(inorbit_api):
     context.mission = mission
     context.options = MissionRuntimeOptions()
     context.shared_memory = MissionRuntimeSharedMemory()
-    tree: BehaviorTree = worker_pool.build_tree_for_mission(context)
+    tree: BehaviorTree = DefaultTreeBuilder().build_tree_for_mission(context)
     logger.info(tree.dump_object())
     expected_obj = {
-        "type": "BehaviorTreeErrorHandler",
-        "state": "",
-        "label": "mission mission123",
-        "children": [
+        'type': 'BehaviorTreeErrorHandler',
+        'state': '',
+        'label': 'mission mission123',
+        'children': [
             {
-                "type": "BehaviorTreeSequential",
-                "state": "",
-                "label": "mission mission123",
-                "children": [
-                    {"type": "MissionStartNode", "state": "", "label": "mission start"},
-                    {"type": "MissionCompletedNode", "state": "", "label": "mission completed"},
+                'type': 'BehaviorTreeSequential',
+                'state': '',
+                'label': 'mission mission123',
+                'children': [
                     {
-                        "type": "UnlockRobotNode",
-                        "state": "",
-                        "label": "unlock robot after mission completed",
+                        'type': 'MissionInProgressNode',
+                        'state': '',
+                        'label': 'mission start'
                     },
-                ],
+                    {
+                        'type': 'MissionCompletedNode',
+                        'state': '',
+                        'label': 'mission completed'
+                    },
+                    {
+                        'type': 'UnlockRobotNode',
+                        'state': '',
+                        'label': 'unlock robot after mission completed'
+                    }
+                ]
             }
         ],
-        "error_handler": {
-            "type": "BehaviorTreeSequential",
-            "state": "",
-            "label": "error handlers",
-            "children": [
+        'error_handler': {
+            'type': 'BehaviorTreeSequential',
+            'state': '',
+            'label': 'error handlers',
+            'children': [
                 {
-                    "type": "MissionAbortedNode",
-                    "state": "",
-                    "label": "mission aborted",
-                    "status": "error",
+                    'type': 'MissionAbortedNode',
+                    'state': '',
+                    'label': 'mission aborted',
+                    'status': 'error'
                 },
                 {
-                    "type": "UnlockRobotNode",
-                    "state": "",
-                    "label": "unlock robot after mission abort",
-                },
-            ],
-        },
-        "cancelled_handler": {
-            "type": "BehaviorTreeSequential",
-            "state": "",
-            "label": "cancel handlers",
-            "children": [
-                {
-                    "type": "MissionAbortedNode",
-                    "state": "",
-                    "label": "mission cancelled",
-                    "status": "OK",
-                },
-                {
-                    "type": "UnlockRobotNode",
-                    "state": "",
-                    "label": "unlock robot after mission cancel",
-                },
-            ],
-        },
-        "pause_handler": {
-            "type": "BehaviorTreeSequential",
-            "state": "",
-            "label": "pause handlers",
-            "children": [
-                {
-                    "type": "MissionPausedNode",
-                    "state": "",
-                    "label": "mission paused",
+                    'type': 'UnlockRobotNode',
+                    'state': '',
+                    'label': 'unlock robot after mission abort'
                 }
-            ],
+            ]
         },
-        "reset_execution_on_pause": False,
+        'cancelled_handler': {
+            'type': 'BehaviorTreeSequential',
+            'state': '',
+            'label': 'cancel handlers',
+            'children': [
+                {
+                    'type': 'MissionAbortedNode',
+                    'state': '',
+                    'label': 'mission cancelled',
+                    'status': 'OK'
+                },
+                {
+                    'type': 'UnlockRobotNode',
+                    'state': '',
+                    'label': 'unlock robot after mission cancel'
+                }
+            ]
+        },
+        'pause_handler': {
+            'type': 'BehaviorTreeSequential',
+            'state': '',
+            'label': 'pause handlers',
+            'children': [
+                {
+                    'type': 'MissionPausedNode',
+                    'state': '',
+                    'label': 'mission paused'
+                }
+            ]
+        },
+        'reset_execution_on_pause': False
     }
     assert expected_obj == tree.dump_object()
 
@@ -124,8 +129,7 @@ def test_bt_wait():
     context = BehaviorTreeBuilderContext()
     context.mission = mission
     context.options = MissionRuntimeOptions()
-    worker_pool = WorkerPool(db=DummyDB(), api=inorbit_api)
-    tree: BehaviorTree = worker_pool.build_tree_for_mission(context)
+    tree: BehaviorTree = DefaultTreeBuilder().build_tree_for_mission(context)
     tree_obj = tree.dump_object()
     # Since we know the structure of the tree (tested in the first test), only fetch the node
     # corresponding to the step created
@@ -154,8 +158,7 @@ def test_bt_wait_expression():
     context.robot_api_factory = RobotApiFactory(api)
     context.mission = mission
     context.options = MissionRuntimeOptions()
-    worker_pool = WorkerPool(db=DummyDB(), api=inorbit_api)
-    tree: BehaviorTree = worker_pool.build_tree_for_mission(context)
+    tree: BehaviorTree = DefaultTreeBuilder().build_tree_for_mission(context)
     tree_obj = tree.dump_object()
     # Since we know the structure of the tree (tested in the first test), only fetch the node
     # corresponding to the step created
@@ -188,8 +191,7 @@ def test_bt_set_data():
     context = BehaviorTreeBuilderContext()
     context.mission = mission
     context.options = MissionRuntimeOptions()
-    worker_pool = WorkerPool(db=DummyDB(), api=inorbit_api)
-    tree: BehaviorTree = worker_pool.build_tree_for_mission(context)
+    tree: BehaviorTree = DefaultTreeBuilder().build_tree_for_mission(context)
     tree_obj = tree.dump_object()
     # Since we know the structure of the tree (tested in the first test), only fetch the node
     # corresponding to the step created
