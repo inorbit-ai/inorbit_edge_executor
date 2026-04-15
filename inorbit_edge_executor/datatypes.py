3fe816486dd1b2363b6b1080e337dba696e89b13
Irina <irina@inorbit.ai>
2026-04-15 11:55:54 -0300

    redefined edge schema

--- inorbit_edge_executor/datatypes.py
+++ inorbit_edge_executor/datatypes.py
@@ -133,10 +133,7 @@ class EdgeCorridor(BaseModel):
 
 
 class Edge(BaseModel):
-    edge_id: Optional[str] = Field(alias="edgeId", default=None)
-    start_waypoint_id: Optional[str] = Field(alias="startWaypointId", default=None)
-    end_waypoint_id: Optional[str] = Field(alias="endWaypointId", default=None)
-    bidirectional: Optional[bool] = Field(default=None)
+    routeId: str
     trajectory: Optional[EdgeTrajectory] = Field(default=None)
     corridor: Optional[EdgeCorridor] = Field(default=None)
     properties: Optional[Dict[str, Dict[str, Optional[str]]]] = Field(default=None)
